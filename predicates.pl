35f9bd424f221d6cad34ece69607744457d0124e
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-02-15 07:27:28 +0300

    added task 1.7 (+ some predicates're modifed with 'fail')

--- predicates.pl
+++ predicates.pl
@@ -1,9 +1,9 @@
 % task 1.1
-men(X):-man(X),write(X),nl.
-women(X):-woman(X),write(X),nl.
+men(X):-man(X),write(X),nl,fail.
+women(X):-woman(X),write(X),nl,fail.
 
 % task 1.2
-children(X):-parent(X,Y),write(Y),nl.
+children(X):-parent(X,Y),write(Y),nl,fail.
 
 % task 1.3
 mother(X,Y):-parent(X,Y),woman(X).
@@ -11,12 +11,16 @@ mother(X):-parent(Y,X),woman(Y),write(Y).
 
 % task 1.4
 daughter(X,Y):-parent(Y,X),woman(X).
-daughter(X):-parent(X,Y),woman(Y),write(Y),fail.
+daughter(X):-parent(X,Y),woman(Y),write(Y).
 
 % task 1.5
 brother(X,Y):-parent(Z,Y),parent(Z,X),man(Z),man(X),not(X=Y).
-brothers(X):-brother(Y,X),write(Y),nl.
+brothers(X):-brother(Y,X),write(Y),nl,fail.
 
 % task 1.6
 wife(X,Y):-parent(Y,Z),parent(X,Z),woman(X).
 wife(X):-man(X),wife(Y,X),write(Y).
+
+% task 1.7
+b_s(X,Y):-parent(Z,X),parent(Z,Y),woman(Z),not(X=Y).
+b_s(X):-b_s(X,Y),write(Y),nl,fail.
