956c52efd7ec7e720e565ac40fd1801e9ab433a5
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-02-15 04:55:08 +0300

    added task 1.4

--- predicates.pl
+++ predicates.pl
@@ -1,7 +1,15 @@
+% task 1.1
 men(X):-man(X),write(X),nl.
 women(X):-woman(X),write(X),nl.
 
+% task 1.2
 children(X):-parent(X,Y),write(Y),nl.
 
+% task 1.3
 mother(X,Y):-parent(X,Y),woman(X).
 mother(X):-parent(Y,X),woman(Y),write(Y).
+
+% task 1.4
+daughter(X,Y):-parent(Y,X),woman(X).
+daughter(X):-parent(X,Y),woman(Y),write(Y),fail.
+
