325ab7fcb314727162ba3b11cd91690f7bc962d0
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-02-28 08:59:10 +0300

    added task 3.7 and tasks description

--- laboratory work №3/recursion.pl
+++ laboratory work №3/recursion.pl
@@ -1,33 +1,37 @@
-% task 3.1
+% task 3.1 - Z максимальное из чисел X и Y
 max(X,Y,Z):-X>=Y,Z is X,!.
 max(_,Y,Y).
 
-% task 3.2
+% task 3.2 - Z максимальное из чисел X, Y и U
 max(X,Y,U,Z):-X>=Y,X>=U,Z is X,!.
 max(_,Y,U,Z):-Y>=U,Z is Y,!.
 max(_,_,U,U).
 
-% task 3.3
+% task 3.3 - факториал через рекурсию вверх
 % fact(1,1):-!.
 % fact(N,X):-	CurN is N-1,fact(CurN,CurX),
 %		X is CurX*N.
 
-% task 3.4
+% task 3.4 - факториал через рекурсию вниз
 extraFact(1,CurX,CurX):-!.
 extraFact(N,CurX,X):-	NewX is CurX*N,CurN is N-1,
 			extraFact(CurN,NewX,X).
 fact(N,X):-extraFact(N,1,X).
 
-% task 3.5
+% task 3.5 - фибоначчи через рекурсию вверх
 % fib(1,1):-!.
 % fib(2,1):-!.
 % fib(N,X):-	PrevX is N-2,fib(PrevX,X1),
 %		NextX is N-1,fib(NextX,X2),
 %		X is X1+X2.
 
-% task 3.6
+% task 3.6 - фибоначчи через рекурсию вниз
 extraFib(1,_,NextX,NextX):-!.
 extraFib(2,_,NextX,NextX):-!.
 extraFib(N,PrevX,NextX,X):-	CurX is PrevX+NextX,CurN is N-1,
 				extraFib(CurN,NextX,CurX,X).
 fib(N,X):-extraFib(N,1,1,X).
+
+% task 3.7 - сумма цифр числа через рекурсию вверх
+sumDigits(0,0):-!.
+sumDigits(N,X):-CurN is N div 10,sumDigits(CurN,CurX),X is CurX + N mod 10. 
