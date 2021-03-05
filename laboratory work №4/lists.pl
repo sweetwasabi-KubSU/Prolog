639506d712e9c6a6138982e726cc4ee5cf786368
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-03-06 01:00:27 +0300

    added task 4.6

--- laboratory work №4/lists.pl
+++ laboratory work №4/lists.pl
@@ -22,22 +22,23 @@ writeList([]):-!.
 writeList([H|T]):-writeln(H),writeList(T).
 
 % task 4.2 - сумма элементов списка (через рекурсию вниз)
-% если Sum - инициализирована, то проверить, правильно ли
+% *+ проверка на правильность заданного значения*
 sum_list_down([],CurSum,CurSum):-!.
 sum_list_down([H|T],CurSum,Sum):-	NewSum is CurSum+H,
 					sum_list_down(T,NewSum,Sum).
 sum_list_down(List,Sum):-	readList(List),
 				sum_list_down(List,0,Sum).
 
-% task 4.3 - сумма элементов списка (через рекурсию вверх),
-% если Sum - инициализирована, то проверить, правильно ли
-sum_list_up([],0):-!.
+% task 4.3 - сумма элементов списка (через рекурсию вверх)
+% *+ проверка на правильность заданного значения*
+sum_list_up([],_):-fail,!.
+sum_list_up([H],H):-!.
 sum_list_up([H|T],Sum):-	sum_list_up(T,CurSum),
 				Sum is CurSum+H.
 
-% task 4.4 - если дан Elem - запись в Num номер первого вхождения
-% если дан Num - запись в Elem значения по этому номеру
-% если даны Elem и Num - проверить, правильно ли
+% task 4.4 - если инициализирован Elem, то Num - номер первого вхождения
+% если инициализирован Num, то Elem - элемент с этим номером
+% *+ проверка на правильность заданных значений*
 list_el_numb([],_,_,_):-fail,!.
 list_el_numb([H|_],H,CurNum,CurNum):-!.
 list_el_numb([_|T],Elem,CurNum,Num):-	NewNum is CurNum+1,
@@ -46,16 +47,25 @@ list_el_numb(List,Elem,Num):-list_el_numb(List,Elem,1,Num).
 
 % task 4.*4 - чтение списка, чтение элемента
 % найти номер первого вхождения элемента в список
+% *+ проверка на правильность*
 predicate4:-	readList(List),
 		write("set the item: "),read(Item),
 		(list_el_numb(List,Item,Number) ->
 		write("first item number: "),write(Number);
 		write("sorry: item isn't in the list!")).
 
-% task 4.5 - чтение списка, чтение номера 
-% найти по номеру элемент в списке
+% task 4.5 - чтение списка, чтение номера элемента
+% найти элемент в списке по заданному номеру
+% *+ проверка на правильность*
 predicate5:-	readList(List),
 		write("set the number: "),read(Number),
 		(list_el_numb(List,Item,Number) ->
 		write("item by number: "),write(Item);
 		write("sorry: item with this number isn't in the list!")).
+
+% task 4.6 - найти минимальный элемент списка (через рекурсию вверх)
+% *+ проверка на правильность*
+min_list_up([],_):-fail,!.
+min_list_up([H],H):-!.
+min_list_up([H|T],Min):-	min_list_up(T,CurMin),
+				(H<CurMin -> Min=H;Min=CurMin).
