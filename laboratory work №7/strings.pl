dd687e872654dc9f70c154f58f590dadaededb55
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-03-21 10:04:50 +0300

    added task 7.2

--- laboratory work №7/strings.pl
+++ laboratory work №7/strings.pl
@@ -3,8 +3,8 @@
 read_string(10,L,L,N,N):-!.	% 10 - enter
 read_string(X,L,CurL,N,CurN):-	NewN is CurN+1,append(CurL,[X],NewL),
 				get0(Y),read_string(Y,L,NewL,N,NewN).
-read_string(L,N):-	get0(X),
-			read_string(X,L,[],N,0).
+read_string(L,N):-	nl,write("input string: "),
+			get0(X),read_string(X,L,[],N,0).
 
 % вывод строки через список ASCII кодов символов
 
@@ -15,13 +15,41 @@ write_string([H|T]):-	put(H),
 % task 7.1 - вывести строку три раза через запятую,
 % показать количество символов в ней 
 
-predicate1:-	nl,write("input string: "),
-		read_string(L,N),nl,
+predicate1:-	read_string(L,N),nl,
 
-		write("output string (three times): "),nl,
+		writeln("output string (three times): "),
 		write_string(L),write(", "),
 		write_string(L),write(", "),
 		write_string(L),nl,nl,
 
 		write("string length: "),writeln(N),nl.
 
+% task 7.2 - найти количество слов в строке
+
+predicate2:-	read_string(L,_),
+		count_words(L,Count),
+		write("number of words: "),
+		writeln(Count),nl.
+
+% считает количество слов в строке
+count_words([],CurCount,CurCount):-!.
+count_words(L,CurCount,Count):-	skip_spaces(L,CurL),
+				get_word(CurL,NewL,Word),
+				Word \= [],
+				NewCount is CurCount+1,
+				count_words(NewL,NewCount,Count),!.
+count_words(_,CurCount,CurCount).
+count_words(L,Count):-count_words(L,0,Count).
+
+% убирает пробелы в начале строки
+skip_spaces([32|T],NewL):-skip_spaces(T,NewL),!.
+skip_spaces(L,L).
+
+% вычленяет слово в начале строки
+get_word([],[],[]):-!.
+get_word(L,NewL,Word):-get_word(L,NewL,Word,[]).
+
+get_word([],[],Word,Word).
+get_word([32|T],T,Word,Word):-!.
+get_word([H|T],NewL,Word,CurWord):-	append(CurWord,[H],NewWord),
+					get_word(T,NewL,Word,NewWord).
