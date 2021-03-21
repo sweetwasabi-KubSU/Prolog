790f2956e3b8338a9df5b0ddee3ee834b62ab332
sweetwasabi-KubSU <gitsweetwasabi@gmail.com>
2021-03-21 12:18:48 +0300

    added task 7.3

--- laboratory work №7/strings.pl
+++ laboratory work №7/strings.pl
@@ -1,5 +1,4 @@
 % чтение строки и преобразование её в список ASCII кодов символов
-
 read_string(10,L,L,N,N):-!.	% 10 - enter
 read_string(X,L,CurL,N,CurN):-	NewN is CurN+1,append(CurL,[X],NewL),
 				get0(Y),read_string(Y,L,NewL,N,NewN).
@@ -7,14 +6,12 @@ read_string(L,N):-	nl,write("input string: "),
 			get0(X),read_string(X,L,[],N,0).
 
 % вывод строки через список ASCII кодов символов
-
 write_string([]):-!.
 write_string([H|T]):-	put(H),
 			write_string(T).
 
 % task 7.1 - вывести строку три раза через запятую,
 % показать количество символов в ней 
-
 predicate1:-	read_string(L,N),nl,
 
 		writeln("output string (three times): "),
@@ -25,21 +22,20 @@ predicate1:-	read_string(L,N),nl,
 		write("string length: "),writeln(N),nl.
 
 % task 7.2 - найти количество слов в строке
-
 predicate2:-	read_string(L,_),
 		count_words(L,Count),
 		write("number of words: "),
 		writeln(Count),nl.
 
 % считает количество слов в строке
-count_words([],CurCount,CurCount):-!.
-count_words(L,CurCount,Count):-	skip_spaces(L,CurL),
+count_words([],Count,Count):-!.
+count_words(L,Count,CurCount):-	skip_spaces(L,CurL),
 				get_word(CurL,NewL,Word),
 				Word \= [],
 				NewCount is CurCount+1,
-				count_words(NewL,NewCount,Count),!.
-count_words(_,CurCount,CurCount).
-count_words(L,Count):-count_words(L,0,Count).
+				count_words(NewL,Count,NewCount),!.
+count_words(_,Count,Count).
+count_words(L,Count):-count_words(L,Count,0).
 
 % убирает пробелы в начале строки
 skip_spaces([32|T],NewL):-skip_spaces(T,NewL),!.
@@ -53,3 +49,38 @@ get_word([],[],Word,Word).
 get_word([32|T],T,Word,Word):-!.
 get_word([H|T],NewL,Word,CurWord):-	append(CurWord,[H],NewWord),
 					get_word(T,NewL,Word,NewWord).
+
+% task 7.3 - определить самое встречаемое слово
+predicate3:-	read_string(L,_),
+
+		get_words(L,Words,_),
+		unique_list(Words,UniqueW),
+
+		count_reps(UniqueW,Counts,Words),
+		max_list_down(Counts,MaxC),
+
+		list_el_numb(Counts,MaxC,Index),
+		list_el_numb(UniqueW,MaxW,Index),
+
+		nl,writeln("*if the frequency of occurrence is repeated,*"),
+		writeln("*the first word is output*"),
+
+		nl,write("the most common word: "),write_string(MaxW),nl,
+		write("frequency of occurrance: "),writeln(MaxC),nl.
+
+% собирает список из слов
+get_words([],Words,Words,C,C):-!.
+get_words(L,Words,CurWords,C,CurC):-	skip_spaces(L,CurL),
+					get_word(CurL,NewL,Word),
+					Word \=[],
+					NewC is CurC+1,
+					append(CurWords,[Word],NewWords),
+					get_words(NewL,Words,NewWords,C,NewC),!.
+get_words(_,Words,Words,C,C).
+get_words(L,Words,Count):-get_words(L,Words,[],Count,0).
+
+% считает количество вхождений элементов из одного списка в другой список
+count_reps([],[],_):-!.
+count_reps([Word|T],Counts,Words):-	count_reps(T,CurCounts,Words),
+					number_times(Words,Count,Word),
+					append([Count],CurCounts,Counts).
